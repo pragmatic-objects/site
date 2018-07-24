3d93a4a6fde5ddfc0bb5b94187f29827fd7abf7f
Kapralov Sergey <skapralov@mail.ru>
2018-07-25 01:16:48 +0300

    Adopted to different screen sizes

--- typographist.js
+++ typographist.js
@@ -3,9 +3,24 @@ const { typographist, ratios } = require('typographist');
 module.exports = () => ({
     plugins: [
         typographist({
-            base: '18px',
+            base: '12px',
             lineHeight: 1.5,
-            ratio: ratios.MINOR_THIRD
+            ratio: ratios.MAJOR_SECOND,
+            mobile: {
+                breakpoint: '320px',
+                base: '14px',
+                ratio: ratios.MINOR_THIRD
+            },
+            tablet: {
+                breakpoint: '768px',
+                base: '16px',
+                ratio: ratios.MINOR_THIRD
+            },
+            desktop: {
+                breakpoint: '1000px',
+                base: '18px',
+                ratio: ratios.MINOR_THIRD
+            }
         })
     ]
 });
