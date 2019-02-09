369e8a3b06a28b46320fc3000cae9ca16c9e519a
Kapralov Sergey <skapralov@mail.ru>
2019-02-09 18:26:14 +0300

    Fixed baselines and added fonts

--- typographist.js
+++ typographist.js
@@ -4,22 +4,22 @@ module.exports = () => ({
     plugins: [
         typographist({
             base: '12px',
-            lineHeight: 2, 
-            ratio: ratios.MAJOR_SECOND,
+            lineHeight: 1.5, 
+            ratio: ratios.GOLDEN_SECTION,
             mobile: {
                 breakpoint: '20em',
                 base: '14px',
-                ratio: ratios.MINOR_THIRD
+                ratio: ratios.GOLDEN_SECTION
             },
             tablet: {
                 breakpoint: '48em',
                 base: '16px',
-                ratio: ratios.MINOR_THIRD
+                ratio: ratios.GOLDEN_SECTION
             },
             desktop: {
                 breakpoint: '64em',
-                base: '20px',
-                ratio: ratios.MINOR_THIRD
+                base: '18px',
+                ratio: ratios.GOLDEN_SECTION
             }
         })
     ]
